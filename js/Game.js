165357b130f437bb6c55ad5f1b231df3f9a12e71
TeddyPgadie <48135408+TeddyPgadie@users.noreply.github.com>
2020-07-16 19:25:31 -0400

    Update Class & cocstructors
    Created a Class and constructors in each class
    Assigned values to the Game.js class's properties

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,10 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+class Game {
+  constructor() {
+    this.missed = 0;
+    this.phrase = [];
+    this.activePhrase = null;
+  }
+}
