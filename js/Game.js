48c4e8d618fe8fc3c1c25bbf520dc74b1918dfdd
TeddyPgadie <48135408+TeddyPgadie@users.noreply.github.com>
2020-07-29 09:01:08 -0400

    Created the {createPhrases()} function

--- js/Game.js
+++ js/Game.js
@@ -7,6 +7,19 @@ class Game {
     this.phrase = [];
     this.activePhrase = null;
   }
-
-  //create a function :createPhrases() 
+  /*
+   * Creates phrases for use in game
+   * @return {array} An array of phrases that could be used in the game
+   */
+  createPhrases = function () {
+    const array = [
+      "Phrase : life is like a box of chocolates",
+      "Phrase : there is no trying",
+      "Phrase : may the force be with you",
+      "Phrase : you have to see the matrix for yourself",
+      "Phrase : you talking to me",
+    ];
+    return array;
+  };
+  phrase = [createPhrases()];
 }
